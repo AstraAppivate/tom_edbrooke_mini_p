db00020d1b0bc036205aeadc30f4466a79643732
Tom Edbrooke <thomas.edbrooke@appivate.mod.uk>
2024-06-05 09:11:24 +0100

    kata9

---
+++ skeleton_broken/board.ts
@@ -0,0 +1,23 @@
+// Make your changes to store and game state in this file
+
+export type Player = 'nought' | 'cross' | 'nobody' | undefined
+export type Cell = number | Player | null
+
+let board:Cell [][]=[]
+
+
+// Return the current board state with either a noughtText or a crossText in
+// each position. Put a null in a position that hasn't been played yet.
+
+//retrieve the board
+export function getBoard(): Cell[][] { //colon shows it's returning a 2D array as it's after the function
+  console.log('getBoard was called!!!!!')
+return board
+}
+
+//overwrite board with new board
+export function setBoard(newBoard: Cell[][]) {
+
+  board = newBoard
+  console.log('setBoard was called')
+}
